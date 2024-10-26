225ec25cf1c24d1164e2cbe132a899d56a1d8572
jessl314 <jlchrysanthemum456@gmail.com>
2024-10-26 15:10:04 -0700

    added player movement
    able to change directions and animation

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
 
@@ -7,8 +8,14 @@ public class PlayerMovement : MonoBehaviour {
 
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    Animator animator;
+    public Animator animator;
     Vector2 movement;
+    public SpriteRenderer renderer;
+    public Sprite left;
+    public Sprite right;
+    public Sprite front;
+    //renderer.sprite = left
+    //
 
 
     // Start is called before the first frame update
@@ -20,11 +27,26 @@ public class PlayerMovement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        // * Time.deltaTime * moveSpeed
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        //transform.position = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+        if (movement.x < 0)
+        {
+            renderer.sprite = left;
+
+        }
+        if (movement.x > 0)
+        {
+            renderer.sprite = right;
+        }
+        if (movement.x == 0)
+        {
+            renderer.sprite = front;
+        }
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
 
 
 
