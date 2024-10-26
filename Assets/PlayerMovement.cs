126dc976897f5a6bcc4a590ac0717a894841f918
jessl314 <jlchrysanthemum456@gmail.com>
2024-10-26 13:46:07 -0700

    hi

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour {
 
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
+    Animator animator;
     Vector2 movement;
 
 
@@ -21,7 +22,12 @@ public class PlayerMovement : MonoBehaviour {
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+
     }
 
     void FixedUpdate()
