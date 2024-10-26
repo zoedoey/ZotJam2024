e2a0a9ad09776d926157aa253c6a916a2a35eee7
jessl314 <jlchrysanthemum456@gmail.com>
2024-10-26 16:12:35 -0700

    added some comments and created enemy movement

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -21,8 +21,55 @@ public class EnemyMovement : MonoBehaviour
     // INVERT UP AND DOWN
 
     // Update is called once per frame
-    void Update()
+    void Update() // updates the players movement
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        //transform.position = new Vector2(transform.position.x + movement.x, transform.position.y + movement.y);
+        if (movement.x < 0)
+        {
+            renderer.sprite = left;
+
+        }
+        if (movement.x > 0)
+        {
+            renderer.sprite = right;
+        }
+        if (movement.x == 0)
+        {
+            renderer.sprite = front;
+        }
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+    }
+    void FixedUpdate() //fixes the update in terms of time/speed
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    //Bullet mechanics
+    public Transform BulletSpawnPoint;
+    public GameObject bullet;
+    public Vector2 up = new Vector2(0, 1);
+    public float force = 10f;
+
+    void Shoot()
+    {
+        GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+        newBullet.GetComponent<Rigidbody2D>().velocity = movement.normalized * force;
+        if (movement.x == 0 && movement.y == 0)
+        {
+            // Need to get this to work, should shoot up when player isn't moving
+            newBullet.GetComponent<Rigidbody2D>().velocity = up * force;
+        }
+        Destroy(newBullet, 3);
     }
 }
+
