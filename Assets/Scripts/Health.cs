a42e91fe46d9ab79d6ea72a818c0560c9dd924e3
jessl314 <jlchrysanthemum456@gmail.com>
2024-10-27 00:00:27 -0700

    code for the hearts I think?
    have not figured out how to attach to any mechanics yet but the code is good. idk why it doesn't show up on game though

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -12,5 +12,27 @@ public class Health : MonoBehaviour
     public Image[] hearts;
     public Sprite fullHeart;
     public Sprite emptyHeart;
+    void Update()
+    {
+        
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            } else {
+                hearts[i].sprite = emptyHeart;
+                
+            }
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
 
 }
