20727b0fc129a2dcd35f5a30d1eac46c0a1734f2
jessl314 <jlchrysanthemum456@gmail.com>
2024-10-27 14:01:05 -0700

    changed the collision for crack
    red crack + enemy = crack disappear

--- Assets/Scripts/CrackCollisionP1.cs
+++ Assets/Scripts/CrackCollisionP1.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
+// implement the bullet speed increase
+// also a win condition for the enemy hits
+
 public class CrackCollisionP1 : MonoBehaviour
 {
+
+    public SpriteRenderer renderer;
+    public Sprite redCrack;
+
     void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(this.gameObject);
+        if (renderer.sprite == redCrack)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
 
