404fbb3e2651b74a6617b4c7f65d9dcc6178a03a
jessl314 <jlchrysanthemum456@gmail.com>
2024-10-26 23:06:26 -0700

    added a health script
    added some hearts and a canvas object

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+}
