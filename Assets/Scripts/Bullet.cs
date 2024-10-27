d91257d473da69fcaeeacc3eb5334f0e51153dcd
zoedoey <zoes1@uci.edu>
2024-10-26 22:56:00 -0700

    Finished bullet spawner
    Bullets still don't do anything

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float bulletLife = 1f;
+    public float bulletSpeed = 1f;
+    public float rotation = 0f;
+    public float timer = 0f;
+    public Vector2 spawnPoint;
+
+    void Start()
+    {
+        spawnPoint = new Vector2(transform.position.x, transform.position.y);
+    }
     
+    void Update()
+    {
+        if (timer > bulletLife)
+        {
+            Destroy(this.gameObject);
+        }
+
+        timer += Time.deltaTime;
+        transform.position = Movement(timer);
+    }
+
+    Vector2 Movement(float timer)
+    {
+        float x = timer * bulletSpeed * transform.right.x;
+        float y = timer * bulletSpeed * transform.right.y;
+
+        return new Vector2(x + spawnPoint.x, y + spawnPoint.y);
+    }
 }
